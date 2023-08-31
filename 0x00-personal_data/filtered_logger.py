ea339732cb73b8478768335d5dff0a8b61a8bfcc
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-31 11:09:11 +0100

    fix requirement error

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,13 +13,11 @@ def filter_datum(fields: List[str], redaction: str,
         fields: A list of strings representing all fields to obfuscate.
         redaction: A string representing by what the field will be obfuscated.
         message: A string representing the log line.
-        separator: A string representing by which character is separating all fields in the log line (message).
+        separator: A string representing by which character
+         is separating all fields in the log line (message).
 
     Returns:
         The obfuscated log message.
     """
-    for field in fields:
-        pattern: str = r'({}=).*?;'.format(field)
-        replace: str = r'\1{}{}'.format(redaction, separator)
-        message = re.sub(pattern, replace, message)
-    return message
+    return re.sub(fr'({"|".join(map(re.escape, fields))})=.*?;',
+                  fr'\1={redaction}{separator}', message)
