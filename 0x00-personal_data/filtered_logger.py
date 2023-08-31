9546c67ccd61df66c0647f4a8804a39f14774286
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-31 12:00:37 +0100

    Make function shorter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,16 +8,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str):
     """
     Obfuscates the sensitive fields in a log message.
-
-    Args:
-        fields: A list of strings representing all fields to obfuscate.
-        redaction: A string representing by what the field will be obfuscated.
-        message: A string representing the log line.
-        separator: A string representing by which character
-         is separating all fields in the log line (message).
-
-    Returns:
-        The obfuscated log message.
     """
     return re.sub(fr'({"|".join(map(re.escape, fields))})=.*?;',
                   fr'\1={redaction}{separator}', message)
