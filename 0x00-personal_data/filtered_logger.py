833f3b3b3900bad51ad6b22d5e668f95b0bb5aaf
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-08-31 22:29:56 +0100

    Update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Contains a filtering function"""
+
 import re
 from typing import List
 
@@ -9,5 +10,9 @@ def filter_datum(fields: List[str], redaction: str,
     """
     Obfuscates the sensitive fields in a log message.
     """
-    return re.sub(fr'({"|".join(map(re.escape, fields))})=.*?;',
-                  fr'\1={redaction}{separator}', message)
+    for field in fields:
+        if field in message:
+            message = re.sub(r"{}=.*?{}".format(field, separator),
+                             '{}={}{}'.format(field, redaction, separator),
+                             message)
+    return message
