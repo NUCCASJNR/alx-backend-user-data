04fa796e59321f13d46a5b2411092d2ef8e9e893
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-04 18:13:13 +0100

    Fixed pycodestyle

--- 0x01-Basic_authentication/models/base.py
+++ 0x01-Basic_authentication/models/base.py
@@ -126,6 +126,7 @@ class Base():
         """ Search all objects with matching attributes
         """
         s_class = cls.__name__
+
         def _search(obj):
             if len(attributes) == 0:
                 return True
