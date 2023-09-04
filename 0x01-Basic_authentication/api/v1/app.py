aac307eb0c6476502e53d21c6f3a66859c962792
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-04 18:20:01 +0100

    remove debug

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -34,4 +34,4 @@ def unauthorized(error) -> tuple[Response, int]:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port, debug=True)
+    app.run(host=host, port=port)
