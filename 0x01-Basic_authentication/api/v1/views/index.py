7212bd4b892ea859a587317c419de4f236ffd7a1
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-04 23:39:18 +0100

    Abort 401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    Handles 401 error
+    :return: return 401 error
+    """
+    abort(401)
