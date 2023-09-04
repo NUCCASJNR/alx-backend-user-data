16ae3ebd32617d0ae47f8e289de35e823a88a2f7
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-04 23:43:47 +0100

    Fixed presistent pycodestyle error

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -30,6 +30,7 @@ def unauthorized(error):
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
