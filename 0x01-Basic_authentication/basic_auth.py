b75d97c58533ccdfbaf0c0c2b2c1c203472859b1
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-04 12:22:36 +0100

    Flask-Auth

---
+++ 0x01-Basic_authentication/basic_auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This module implements the basic auth feature of authentication"""
+from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "john": generate_password_hash('idan'),
+    "susan": generate_password_hash('idanre')
+}
+
+
+@auth.verify_password
+def verify_password(username: str, password: str):
+    if username in users and check_password_hash(users.get(username), password):
+        return username
+    return None
+
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, {}!".format(auth.current_user())
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ 0x01-Basic_authentication/digest_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""This module implements the digest auth form of authentication"""
+from flask import Flask
+from flask_httpauth import HTTPDigestAuth
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key here'
+auth = HTTPDigestAuth()
+
+users = {
+    "john": "hello",
+    "susan": "bye"
+}
+
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hello, {}!".format(auth.username())
+
+
+if __name__ == '__main__':
+    app.run()
