24d921fc51a4b4ae8b6218d0aac9cab60584bbbb
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-04 22:47:06 +0100

    Changed return data type

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort
+from flask import jsonify, abort, Response
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
+def status() -> Response:
     """ GET /api/v1/status
     Return:
       - the status of the API
