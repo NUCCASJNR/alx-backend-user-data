6f2ec48317c4c10d26728301ee9e52d442fece89
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-05 14:52:12 +0100

    Basic Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,19 @@ class BasicAuth(Auth):
     """
     Basic Auth class
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts Base 64 from Authorization header for Basic Authentication.
+        :param authorization_header: The Authorization header string.
+        :return: The Base 64 part of the header,
+         or None if it doesn't meet the criteria.
+        """
+        if not authorization_header:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        parts = authorization_header.split()
+        if len(parts) == 2 and parts[0] == 'Basic':
+            return parts[1]
+        return None
