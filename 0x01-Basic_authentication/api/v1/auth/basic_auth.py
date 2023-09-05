8a101ea1f17c51cbf77f0cbdb7d7935827114909
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-05 17:50:30 +0100

    Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 """Contains a BasicAuth class that inherits from auth"""
+import binascii
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,3 +27,24 @@ class BasicAuth(Auth):
         if len(parts) == 2 and parts[0] == 'Basic':
             return parts[1]
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Decoded value
+        :param base64_authorization_header: value to be decoded
+        :return: None if base64_authorization_header is None
+        if base64_authorization_header is not a string
+            None if base64_authorization_header is not a valid Base64
+        """
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_header = base64.b64decode(base64_authorization_header)
+            if decoded_header:
+                return decoded_header.decode('utf-8')
+            return None
+        except binascii.Error:
+            return None
