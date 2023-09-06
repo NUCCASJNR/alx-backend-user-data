48a8cf419f783ca72bd1f808461ca973a5e7fc1b
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-06 16:36:07 +0100

    session auth based on basic auth

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -68,6 +68,7 @@ def before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
