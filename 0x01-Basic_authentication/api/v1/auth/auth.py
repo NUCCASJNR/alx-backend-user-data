7adce50bc830d8abda4a196b686be4fce07267c7
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-06 11:45:46 +0100

    this should  work

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -37,9 +37,8 @@ class Auth:
                 stripped = normalized_excluded_path.rstrip('*')
                 if stripped == normalized_path:
                     return False
-            return False
-            if normalized_path == normalized_excluded_path:
-                return False
+                if normalized_path == normalized_excluded_path:
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
