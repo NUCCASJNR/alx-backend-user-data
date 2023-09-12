557fb48ffd7ba3223c82e56340957bfeb175fe25
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-12 17:56:06 +0100

    another style

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,14 +72,14 @@ class Auth:
         except NoResultFound:
             return False
 
-    def create_session(self, email: str) -> str | None:
+    def create_session(self, email: str) -> str:
         """
         Create session
         """
         try:
             user = self._db.find_user_by(email=email)
-            session_id = _generate_uuid()
-            user.session_id = session_id
-            return session_id
         except NoResultFound:
             return None
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
