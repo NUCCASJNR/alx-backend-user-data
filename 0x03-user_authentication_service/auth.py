fdb96cdfb20ad7386e26ffaaebb6f14da21af6b5
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-12 17:39:44 +0100

    id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,8 +81,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            session_id = _generate_uuid()
-            self._db.update_user(user.id, session_id=session_id)
-            return session_id
+            id = _generate_uuid()
+            self._db.update_user(user.id, session_id=id)
+            return id
         except NoResultFound:
             return None
