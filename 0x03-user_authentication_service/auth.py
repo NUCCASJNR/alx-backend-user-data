bba8f6120159dc92edc95b8dc387721fb08eff3c
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-13 12:27:13 +0100

    raise valueError

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -122,10 +122,9 @@ class Auth:
         :return:
             the generated token
         """
-        try:
-            user = self._db.find_user_by(email=email)
+        user = self._db.find_user_by(email=email)
+        if user:
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
-        except ValueError:
-            raise ValueError
+        raise ValueError
