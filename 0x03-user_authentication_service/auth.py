27e564d95c958619caac668c644f5c5bb54da4ee
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-13 21:23:42 +0100

    sesssion id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,7 +79,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
-            self._db.update_user(user.id, session_id=session_id)
+            user.session_id = session_id
             return user.session_id
         except NoResultFound:
             return None
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,10 +1,112 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-from user import User
 
-print(User.__tablename__)
+"""
+End-to-end integration test
+"""
 
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
+import requests
+
+URL = "http://localhost:5000"
+
+
+def register_user(email: str, password: str) -> None:
+    """
+    test register user
+    :param email: email address
+    :param password: password
+    :return: 
+        user obj
+    """
+    data = {"email": email, "password": password}
+    response = requests.post(f"{URL}/users", data=data)
+    if response.status_code == 400:
+        assert response.json()['message'] == "email already registered"
+    else:
+        assert response.status_code == 200
+        assert response.json().get("email") == email
+        assert response.json().get("message") == "user created"
+        print("User successfully registered")
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """
+    logs in using a wrong password
+    :param email: email
+    :param password: password
+    :return:
+        message
+    """
+    data = {"email": email, "password": password}
+    response = requests.post(f"{URL}/sessions", data=data)
+    assert response.status_code == 401
+    print("Incorrect login attempt handled")
+
+
+def log_in(email: str, password: str) -> str:
+    """
+    Logs in a user
+    :param email: email address
+    :param password: password
+    :return:
+        user obj
+    """
+    data = {"email": email, "password": password}
+    response = requests.post(f"{URL}/sessions", data=data)
+    session_id = response.cookies.get("session_id")
+    assert response.status_code == 200
+    assert response.json().get("email") == email
+    assert response.json().get("message") == "logged in"
+    print("User Successfully logged in")
+    return session_id
+
+
+def profile_unlogged() -> None:
+    """
+    Un logged profile
+    :return: 
+        None
+    """
+    response = requests.get(f"{URL}/profile")
+    assert response.status_code == 403
+    print("Unlogged user")
+
+
+def profile_logged(session_id: str) -> None:
+    """
+    View profile while loggedin
+    :param session_id: session_id
+    :return:
+        user obj
+    """
+    data = {"session_id": session_id}
+    response = requests.get(f"{URL}/profile", cookies=data)
+    assert response.status_code == 200
+    assert response.json().get("email")
+
+
+def log_out(session_id: str) -> None:
+    """
+    log out
+    """
+    cookie = {"session_id": session_id}
+    response = requests.delete(f"{URL}/sessions", cookies=cookie)
+    assert response.history == []
+    assert response.status_code == 403
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    # reset_token = reset_password_token(EMAIL)
+    # update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
