627ae1812b604fc3567330f3c2b1c9abc8c89c80
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-13 12:46:32 +0100

    err

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -124,6 +124,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
+            if not user:
+                raise ValueError
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return user.reset_token
