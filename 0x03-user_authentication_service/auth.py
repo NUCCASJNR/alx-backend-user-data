db72512f5f1a8675c142fa3e1f3826f5f0ff6a54
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-13 12:47:48 +0100

    exception

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -128,6 +128,6 @@ class Auth:
                 raise ValueError
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
-            return user.reset_token
-        except NoResultFound:
+            return reset_token
+        except Exception:
             raise ValueError
