f4e7e00f4d470cb5a01e02f9de48ecc70afdb3d5
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-13 12:59:28 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -109,10 +109,7 @@ class Auth:
         """
         if not user_id:
             return None
-        try:
-            self._db.update_user(user_id, session_id=None)
-        except NoResultFound:
-            return None
+        self._db.update_user(user_id, session_id=None)
 
     def get_reset_password_token(self, email: str) -> str:
         """
