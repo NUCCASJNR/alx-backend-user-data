9a6239955e7061cd26e9d1288fcb63ddaf7dfaeb
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-09-14 22:30:33 +0100

    donald's method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -76,11 +76,10 @@ class Auth:
         """create a session for a user"""
         try:
             user = self._db.find_user_by(email=email)
-            session_id = _generate_uuid()
-            self._db.update_user(user.id, session_id=session_id)
-            return session_id
+            user.session_id = _generate_uuid()
+            return user.session_id
         except NoResultFound:
-            return
+            return None
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """
